0054e0a1f7a970262608725fd8299032099bb09d
unknown <you@example.com>
2020-08-30 12:28:44 +0530

    done

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Product(models.Model):
+	title=models.CharField(max_length=200)
+	pubdate=models.DateTimeField()
+	body=models.TextField()
+	url=models.TextField()
+	image=models.ImageField(upload_to='images/')
+	icon=models.ImageField(upload_to='images/')
+	votes_total=models.IntegerField(default=1)
+	hunter=models.ForeignKey(User,on_delete=models.CASCADE)
 
-# Create your models here.
+	def __str__(self):
+		return self.title
+ 
+	def summary(self):
+		return self.body[:100]
+
+	def pubdate_pretty(self):
+		return self.pubdate.strftime('%b %e %Y')
+	
